a64531ff2fefe8cfe1f2588f66677714b4b74e39
Artem Mesa4 <mesa4elo@gmail.com>
2017-10-30 18:18:16 +0200

    finish header added second section

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,13 @@
 
 $(document).ready(function() {
 
+	$('.header__bottom__container__left__consult--right').on('click', function(){
+		$('.header__bottom__container__left__consult--send-cv').slideToggle(100);
+	});
+
+	$('.header__bottom__container__right__about__item--left').on('mouseenter', function() {
+		$('.header__bottom__container__right__about__item--right.active').removeClass('active');
+		$(this).next().addClass('active');
+	});
+
 });
