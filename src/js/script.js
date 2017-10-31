077b382bd75237a1c6e7deefcbebc5d720f7f9fa
Artem Mesa4 <mesa4elo@gmail.com>
2017-10-31 18:20:41 +0200

    added a new functions + payment section

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,84 @@ $(document).ready(function() {
 		$(this).next().addClass('active');
 	});
 
+	(function() {
+
+		function prof( element ) {
+			$('.professions__salary').css('opacity', '0');
+			$('#professions-list li span.active').removeClass('active');
+			var professionStart = element.find('span').addClass('active');
+			$('.professions__salary .professions__salary__selected--prof').text(professionStart.text());
+			$('.professions__salary .professions__salary__selected--sal').text(professionStart.data('cost'));
+			$('.professions__salary').animate({opacity: 1},400);
+		}
+		prof($('#professions-list li').eq(0));
+
+		$('#professions-list li').on('click', function(){
+			prof( $(this) );
+		});
+	})();
+
+
+	// $('body').on('click',function(e) {
+	// 	if ($(this).has(e.target) == $('.consult')){
+	// 		$('.consult').find('.consult--send-cv').slideToggle(100);
+	// 	} else if ($(this).has(e.target) !== $('.consult')){
+	// 		$('.consult').find('.consult--send-cv').slideToggle(100);
+	// 	}
+	// });
+
+	// $('.consult').on('click',function(e) {
+	// 	e.preventDefault();
+	// 	var rightBtn = $(this).find('.consult--right');
+	// 	if (rightBtn.has(e.target) != 0) {
+	// 		console.log('tyt');
+	// 	}
+	// });
+
+	var swith = false;
+	function addedBodySlide( element ) {
+
+		var element_class = element.attr('class');
+		var consult = element.closest('.consult');
+
+		$('body').on('click',function(e) {
+			e.preventDefault();
+			console.log(1);
+
+			if ( $(e.target).is(element) || $(e.target).closest('.' + element_class).is(element) ) {
+
+				return false;
+			} else {
+				//$('body').unbind('click');
+				 if( element.closest('.js-active') ){
+					 consult.removeClass('js-active');
+					 consult.find('.consult--send-cv').slideUp(100, function(){
+
+					 });
+
+				 }
+
+
+			}
+		});
+	}
+
+	$('.consult--right').on('click', function(e) {
+		e.preventDefault();
+		console.log('01');
+		var consult = $(this).closest('.consult');
+
+		if( consult.hasClass('js-active') ) {
+			consult.removeClass('js-active');
+			consult.find('.consult--send-cv').slideUp(100);
+		} else {
+			consult.addClass('js-active');
+			consult.find('.consult--send-cv').slideDown(100);
+			addedBodySlide( $(this) );
+		}
+
+
+	});
+
+
 });
